f8b73bc5e991ddf375732c0d08915983653adf2f
stouf <ouadad.mustapha01@gmail.com>
2025-04-08 18:01:28 +0100

    generate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectComponent } from './project/project.component';
+import { HeaderComponent } from './header/header.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,
+     RouterOutlet,
+     SkillsComponent,
+     ProjectComponent,
+     HeaderComponent,
+     ExperienceComponent
+    ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
