908a06ac695f09dfb91490da604839d6b4a0f861
stouf <ouadad.mustapha01@gmail.com>
2025-04-13 22:06:17 +0100

    modifier f pareti skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-
-type SkillPosition = [number,number,string];
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -10,44 +8,6 @@ type SkillPosition = [number,number,string];
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
-export class SkillsComponent implements OnInit {
-  skills = ["angular", "java EE","javascript","php","html","css","react","laravel","bootstrap"]
-  skillPosition:SkillPosition[] = [];
-  
- 
-  currentlyDragging : number | undefined;
-
-  mouseX: number | undefined;
-  mouseY: number | undefined;
-
-  ngOnInit() {
-    for (let i = 0; i<this.skills.length;i++ ) {
-      let pos: SkillPosition  = ([ i * (90 / this.skills.length),((i + 1) % 3) * 33, this.getRandomRotation() ]);
-      this.skillPosition.push(pos);
-     
-      
-    }
-    
-  }
-  getRandomRotation(){
-    let randomVal=(Math.random()*30)-15;
-    return `rotate(${randomVal}deg)`;
-  }
-
-  startDragging(i :number){
-    
-    this.currentlyDragging = i;
-  }
-
-  mousemove(e : MouseEvent){
-    if (this.currentlyDragging == undefined) {return;}
-    this.mouseX = e.pageX -40;
-    this.mouseY =e.pageY - 8;
-  }
-
-  stopDragging(){
-    this.currentlyDragging=undefined;
-  }
-
-  
+export class SkillsComponent {
+  skills = ["Angular", "Java EE", "JavaScript", "PHP", "HTML", "CSS", "React", "Laravel", "Bootstrap"];
 }
