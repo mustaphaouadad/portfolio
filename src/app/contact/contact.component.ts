ead2c8cf512c177e47379f1f51343ebc0de06702
stouf <ouadad.mustapha01@gmail.com>
2025-04-13 22:26:15 +0100

    modifier f contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
@@ -21,13 +22,13 @@ export class ContactComponent {
   onSubmit() {
     console.log('Form data:', this.formData);
 
-    // simulation d’envoi
+    
     this.messageEnvoye = true;
 
-    // vider les champs
+   
     this.formData = { name: '', email: '', message: '' };
 
-    // masquer le message après quelques secondes (optionnel)
+    
     setTimeout(() => {
       this.messageEnvoye = false;
     }, 5000);
